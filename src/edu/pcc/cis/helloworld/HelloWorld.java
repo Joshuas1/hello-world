3ddb4e7481747e182e86f828f6fc176143daf00b
Joshua Schultz <josh.schultz2@pcc.edu>
2021-04-26 16:42:45 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,5 @@
 package edu.pcc.cis.helloworld;
-
+import java.util.Date;
 /**
  * @author Joshua Schultz
  * @version 2021.04.26
@@ -7,5 +7,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world and all who inhabit it");
+        System.out.println(new Date());
     }
 }
