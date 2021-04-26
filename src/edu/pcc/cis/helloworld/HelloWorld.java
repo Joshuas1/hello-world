f2c4f89453adca1bbe935273fcd473f685c05c68
Joshua Schultz <josh.schultz2@pcc.edu>
2021-04-26 16:05:20 -0700

    added functionality and author and version tags

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,11 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * @author Joshua Schultz
+ * @version 2021.04.26
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
