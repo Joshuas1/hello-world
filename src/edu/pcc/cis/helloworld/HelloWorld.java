4a00a12cf95001981c28f9b569c63575176c783c
Joshua Schultz <josh.schultz2@pcc.edu>
2021-04-26 16:39:20 -0700

    Added new hello message

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -6,6 +6,6 @@ package edu.pcc.cis.helloworld;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello world and all who inhabit it");
     }
 }
